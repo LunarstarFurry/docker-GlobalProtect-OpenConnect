c987b7d96827f43195bc79ab4ff5163e94639494
LunarstarFurry <lunarstarpony@yahoo.com>
2026-03-19 18:14:26 +0800

    Add VPN connection script for GlobalProtect

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -c /dev/net/tun ]; then
+    mkdir -p /dev/net/tun
+    mknod /dev/net/tun c 10 200
+    chmod 600 /dev/net/tun
+fi
+
+echo "Connecting to GlobalProtect at $VPN_SERVER..."
+
+exec echo "$VPN_PASS" | openconnect \
+    --protocol=gp \
+    --user="$VPN_USER" \
+    --passwd-on-stdin \
+    --script=/usr/share/vpnc/vpnc-script \
+    --non-inter \
+    "$VPN_SERVER"
